86b7ab386d40d5bad6c601bbdff82f5a914d2ffe
Airdata <airda@abv.bg>
2018-03-21 18:09:32 +0200

    mailform

--- email.php
+++ email.php
@@ -1,6 +1,6 @@
 <?php
 $subject = $_REQUEST['subject']; // Subject of your email
-$to = 'airdata@abv.bg';  //Recipient's E-mail
+$to = 'rumenlishkoff@gmail.com';  //Recipient's E-mail
 $headers  = 'MIME-Version: 1.0' . "\r\n";
 $headers .= "From: " . $_REQUEST['email'] . "\r\n"; // Sender's E-mail
 $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
