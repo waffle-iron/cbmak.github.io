2402478bd336631fe45dd2de5e323e5deb3a29b3
Airdata <airda@abv.bg>
2018-03-21 18:05:26 +0200

    fix clouds image

--- email.php
+++ email.php
@@ -1,6 +1,6 @@
 <?php
 $subject = $_REQUEST['subject'] . ' Ajax HTML Contact Form : Demo'; // Subject of your email
-$to = $_REQUEST['email'];  //Recipient's E-mail
+$to = 'airdata@abv.bg';  //Recipient's E-mail
 $headers  = 'MIME-Version: 1.0' . "\r\n";
 $headers .= "From: " . $_REQUEST['email'] . "\r\n"; // Sender's E-mail
 $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
