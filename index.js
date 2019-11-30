2bc8448c075d3befec35e9f762a3d81c51a1170f
airdata <airdata@abv.bg>
2019-11-30 04:35:23 +0200

    change svg

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ http.createServer(function (request, response) {
     fs.readFile(filePath, function(error, content) {
         if (error) {
             if(error.code == 'ENOENT') {
-                fs.readFile('./404.html', function(error, content) {
+                fs.readFile('./error/HTTP404.html', function(error, content) {
                     response.writeHead(404, { 'Content-Type': 'text/html' });
                     response.end(content, 'utf-8');
                 });
